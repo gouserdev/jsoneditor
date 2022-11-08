53ecbd8ac3383ee92edf07e45b13e695c2d3c79e
gouserdev <116933955+gouserdev@users.noreply.github.com>
2022-11-08 06:39:00 -0500

    Update je.go

--- je.go
+++ je.go
@@ -2,8 +2,11 @@ package jsoneditor
 
 import (
 	gin "github.com/gouserdev/jsoneditor/pkg/gin"
+	"github.com/gouserdev/jsoneditor/pkg/util"
 )
 
 func Jsoneditor(ip string, port string) {
+	configpath := "./configs/config.json"
+	util.Open("http://127.0.0.1:10889/j/index.html?pass=" + util.GetFromJson("password", configpath))
 	gin.GinInit(ip, port)
 }
